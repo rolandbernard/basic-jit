5ae8c1cc9168b03c5677fa30bea4b95e3daad228
Roland Bernard <rolli.bernard@gmail.com>
2020-10-09 23:40:29 +0200

    Further testing

--- src/basicjit.c
+++ src/basicjit.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
@@ -9,21 +10,19 @@
 
 typedef int (*JitFunction)();
 
+static uint8_t test_program[] = {
+    0xB8, 0x2a, 0x00, 0x00, 0x00, // mov aex, 42
+    0xe9, 0x05, 0x00, 0x00, 0x00, // jr 5
+    0xB8, 0x0c, 0x00, 0x00, 0x00, // mov aex, 12
+    0xc3, // retn
+};
+
 int main(int argc, char** argv) {
     size_t memory_size = INITIAL_MEMORY_SIZE;
     uint8_t* jit_memory = (uint8_t*)malloc(memory_size * sizeof(uint8_t));
     JitFunction entry = (JitFunction)jit_memory;
 
-    jit_memory[0] = 0x90;
-    jit_memory[1] = 0x90; 
-    jit_memory[2] = 0x90;
-    jit_memory[3] = 0x90; 
-    jit_memory[4] = 0xB8; 
-    jit_memory[5] = 0x2a;
-    jit_memory[6] = 0x00; 
-    jit_memory[7] = 0x00;
-    jit_memory[8] = 0x00; 
-    jit_memory[9] = 0xc3; 
+    memcpy(jit_memory, test_program, sizeof(test_program));
     
     size_t pagesize = sysconf(_SC_PAGESIZE);
     uintptr_t start = (uintptr_t)jit_memory;
@@ -32,7 +31,8 @@ int main(int argc, char** argv) {
     if(mprotect((void*)pagestart, end - pagestart, PROT_EXEC | PROT_READ | PROT_WRITE)) {
         perror("Failed to set memory protection");
     } else {
-        fprintf(stderr, "Return: %i\n", entry());
+        fprintf(stderr, "JIT memory: %p\n", jit_memory);
+        fprintf(stderr, "Return:     %i\n", entry());
     }
     
     free(jit_memory);
