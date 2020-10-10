d08f107b7f4a4cd3b561da9efcb2997400d9747a
Roland Bernard <rolli.bernard@gmail.com>
2020-10-10 16:20:21 +0200

    Added a stack allocator

---
+++ src/stackalloc.c
@@ -0,0 +1,41 @@
+
+#include <stdlib.h>
+
+#include "stackalloc.h"
+
+#define INITIAL_CAPACITY (1 << 20);
+
+void* memory = NULL;
+size_t capacity = 0;
+size_t allocated = 0;
+
+void* alloc_on_stack(size_t size) {
+    if(capacity <= allocated + size) {
+        if(capacity == 0) {
+            capacity = INITIAL_CAPACITY;
+        }
+        while(capacity <= allocated + size) {
+            capacity *= 2;
+        }
+        memory = realloc(memory, capacity);
+    }
+    void* ret = memory + allocated;
+    allocated += size;
+    return ret;
+}
+
+void free_stack_till(void* addr) {
+    allocated = (addr - memory);
+    if(allocated < capacity / 64) {
+        while(allocated < capacity / 64) {
+            capacity /= 2;
+        }
+        memory = realloc(memory, capacity);
+    }
+}
+
+void free_complete_stack() {
+    allocated = 0;
+    capacity = 0;
+    free(memory);
+}
