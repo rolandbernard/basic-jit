783fac8e84387ea7bbcca543e875e9da1acf3e81
Roland Bernard <rolli.bernard@gmail.com>
2020-10-15 22:00:41 +0200

    Enabled greater allocation

--- src/common/stackalloc.c
+++ src/common/stackalloc.c
@@ -27,25 +27,39 @@ void* alloc_unaligned(StackAllocator* mem, size_t size) {
 
 // This is used for ast allocation, where the absolute address must stay constant
 void* alloc_aligned(StackAllocator* mem, size_t size) {
-    if (mem->capacity <= mem->occupied + 2*size) {
-        if (mem->capacity == 0) {
-            mem->memory = malloc(INITIAL_CAPACITY);
+    if(size > INITIAL_CAPACITY) {
+        if(mem->next == NULL) {
+            StackAllocator* next = (StackAllocator*)malloc(sizeof(StackAllocator));
+            next->capacity = size;
+            next->occupied = size;
+            next->memory = malloc(size);
+            next->next = NULL;
+            mem->next = next;
+            return next->memory;
         } else {
-            if (mem->next == NULL) {
-                StackAllocator* next = (StackAllocator*)malloc(sizeof(StackAllocator));
-                next->capacity = 0;
-                next->occupied = 0;
-                next->memory = NULL;
-                next->next = NULL;
-                mem->next = next;
-            }
             return alloc_aligned(mem->next, size);
         }
+    } else {
+        if (mem->capacity <= mem->occupied + 2 * size) {
+            if (mem->capacity == 0) {
+                mem->memory = malloc(INITIAL_CAPACITY);
+            } else {
+                if (mem->next == NULL) {
+                    StackAllocator* next = (StackAllocator*)malloc(sizeof(StackAllocator));
+                    next->capacity = 0;
+                    next->occupied = 0;
+                    next->memory = NULL;
+                    next->next = NULL;
+                    mem->next = next;
+                }
+                return alloc_aligned(mem->next, size);
+            }
+        }
+        mem->occupied = (mem->occupied + 15) & ~(size_t)15;
+        void* ret = mem->memory + mem->occupied;
+        mem->occupied += size;
+        return (void*)ret;
     }
-    mem->occupied = (mem->occupied + 15) & ~(size_t)15;
-    void* ret = mem->memory + mem->occupied;
-    mem->occupied += size;
-    return (void*)ret;
 }
 
 void free_stack(StackAllocator* mem) {
