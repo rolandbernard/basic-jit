98b103d2f32b38879bebdc79de425f4b2abd87a3
Roland Bernard <rolli.bernard@gmail.com>
2020-10-17 21:37:49 +0200

    Added a datalist to store for the 'DATA' statment

---
+++ src/codegen/datalist.c
@@ -0,0 +1,17 @@
+
+#include <stdlib.h>
+
+#include "codegen/datalist.h"
+
+void addDataToList(DataList* list, DataElement entry) {
+    if(list->count == list->capacity) {
+        if(list->capacity == 0) {
+            list->capacity = 16;
+        } else {
+            list->capacity *= 2;
+        }
+        list->data = (DataElement*)realloc(list->data, sizeof(DataElement) * list->capacity);
+    }
+    list->data[list->count] = entry;
+    list->count++;
+}
---
+++ src/codegen/datalist.h
@@ -0,0 +1,23 @@
+#ifndef _DATA_LIST_H_
+#define _DATA_LIST_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef struct {
+    int64_t integer;
+    double real;
+    const char* string;
+} DataElement;
+
+typedef struct {
+    size_t count;
+    size_t capacity;
+    DataElement* data;
+} DataList;
+
+#define DATA_LIST_INITIALIZER { .count = 0, .capacity = 0, .data = NULL }
+
+void addDataToList(DataList* list, DataElement entry);
+
+#endif
