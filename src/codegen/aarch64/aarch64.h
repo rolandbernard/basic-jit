e816ab4da23fb7396f386d9b77a10aa5fded3768
Roland Bernard <rolli.bernard@gmail.com>
2021-01-13 21:17:20 +0100

    Work on defining the aarch64 instruction format

--- src/codegen/aarch64/aarch64.h
+++ src/codegen/aarch64/aarch64.h
@@ -25,8 +25,8 @@ typedef enum {
 typedef enum {
     PROC_IMM_OP0_PC_REL0 = 0b000,
     PROC_IMM_OP0_PC_REL1 = 0b001,
-    PROC_IMM_OP0_ADD_IMM = 0b010,
-    PROC_IMM_OP0_ADD_IMM_TAG = 0b011,
+    PROC_IMM_OP0_ADD = 0b010,
+    PROC_IMM_OP0_ADD_TAG = 0b011,
     PROC_IMM_OP0_LOGICAL = 0b100,
     PROC_IMM_OP0_MOVE = 0b101,
     PROC_IMM_OP0_BITFIELD = 0b110,
@@ -43,10 +43,40 @@ typedef enum {
     ADD_IMM_OP_SUB = 0b1,
 } Aarch64AddImmOp;
 
+typedef enum {
+    LOGICAL_IMM_OPC_AND = 0b00,
+    LOGICAL_IMM_OPC_ORR = 0b01,
+    LOGICAL_IMM_OPC_EOR = 0b10,
+    LOGICAL_IMM_OPC_ANDS = 0b11,
+} Aarch64LogicalImmOpc;
+
+typedef enum {
+    MOVE_IMM_OPC_MOVN = 0b00,
+    MOVE_IMM_OPC_UNALLOCATED = 0b01,
+    MOVE_IMM_OPC_MOVZ = 0b10,
+    MOVE_IMM_OPC_MOVK = 0b11,
+} Aarch64MoveImmOpc;
+
+typedef enum {
+    BITFIELD_OPC_SBFM = 0b00,
+    BITFIELD_OPC_BFM = 0b01,
+    BITFIELD_OPC_UBFM = 0b10,
+    BITFIELD_OPC_UNALLOCATED = 0b11,
+} Aarch64BitfieldOpc;
+
+typedef enum {
+    EXTACT_OP21_UNALLOCATED0 = 0b00,
+    EXTACT_OP21_UNALLOCATED1 = 0b01,
+    EXTACT_OP21_UNALLOCATED2 = 0b10,
+    EXTACT_OP21_EXTR = 0b11,
+} Aarch64ExtractOp21;
+
+#define COND_BRANCH_IMM_PAD0 0b0101010
+
 typedef union {
     struct {
         uint32_t pad1 : 25;
-        Aarch64MainOp op0 : 4;
+        Aarch64MainOp0 op0 : 4;
         uint32_t pad0 : 3;
     } main;
     struct {
@@ -90,6 +120,59 @@ typedef union {
         Aarch64AddImmOp op : 1;
         uint32_t sf : 1;
     } add_imm_tag;
+    struct {
+        uint32_t rd : 5;
+        uint32_t rn : 5;
+        uint32_t imms : 6;
+        uint32_t immr : 6;
+        uint32_t n : 1;
+        uint32_t pad0 : 6;
+        Aarch64LogicalImmOpc opc : 2;
+        uint32_t sf : 1;
+    } logical_imm;
+    struct {
+        uint32_t rd : 5;
+        uint32_t imm16 : 16;
+        uint32_t hw : 2;
+        uint32_t pad0 : 6;
+        Aarch64MoveImmOpc opc : 2;
+        uint32_t sf : 1;
+    } move_imm;
+    struct {
+        uint32_t rd : 5;
+        uint32_t rn : 5;
+        uint32_t imms : 6;
+        uint32_t immr : 6;
+        uint32_t n : 1;
+        uint32_t pad0 : 6;
+        Aarch64BitfieldOpc opc : 2;
+        uint32_t sf : 1;
+    } bitfield;
+    struct {
+        uint32_t rd : 5;
+        uint32_t rn : 5;
+        uint32_t imms : 6;
+        uint32_t rm : 5;
+        uint32_t o0 : 1;
+        uint32_t n : 1;
+        uint32_t pad0 : 6;
+        Aarch64ExtractOp21 op21 : 2;
+        uint32_t sf : 1;
+    } extract;
+    struct {
+        uint32_t op2 : 5;
+        uint32_t pad1 : 7;
+        uint32_t op1 : 14;
+        uint32_t pad0 : 3;
+        uint32_t op0 : 3;
+    } branches;
+    struct {
+        uint32_t cond : 4;
+        uint32_t o0 : 1;
+        uint32_t imm19 : 19;
+        uint32_t o1 : 1;
+        uint32_t pad0 : 7;
+    } cond_branch_imm;
     uint32_t instruction;
 } Aarch64Instruction;
 
